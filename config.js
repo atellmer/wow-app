00b9717e458b88a3ef1fe3c071b93f7cbb9799bf
atellmer <atellmer@gmail.com>
2017-01-26 12:17:15 +0500

    gh pages

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-	root: 'client/',
-	port: 3000,
-	mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
-	debug: process.env.NODE_ENV !== 'production' ? true : false
-}
